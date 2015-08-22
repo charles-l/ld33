2f2c9919d9cb273359b34225d15eecaaa92255ed
charles-l <chuckiels2011@gmail.com>
2015-08-22 11:25:57 -0400

    more things!!!

--- Game.lua
+++ Game.lua
@@ -6,18 +6,26 @@ function Game:load()
     self.entities = {}
     self.entities.level = Level:new(self)
     self.entities.player = Player:new(self, 50, 50)
+    self.cam = gamera.new(0, 152, 2000, 2000)
 end
 
 function Game:draw()
+    love.graphics.setColor(255, 255, 255, 255)
     self.cam:draw(function(l,t,w,h)
         love.graphics.scale(2)
         for _, v in pairs(self.entities) do
             v:draw()
         end
     end)
+    love.graphics.setColor(0, 0, 0, 255)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), 150)
+    love.graphics.rectangle('fill', 0, love.graphics.getHeight() - 150, love.graphics.getWidth(), 150)
 end
 
 function Game:update(dt)
+    local cx, cy = self.pworld:getRect(self.entities.player)
+    self.cam:setPosition(cx * 2, cy * 2) -- follow player
+
     for _, v in pairs(self.entities) do
         if v["update"] ~= nil then
             v:update(dt)
--- Level.lua
+++ Level.lua
@@ -10,20 +10,30 @@ function Level:initialize(game, file)
     self.img = game.res.img["tilesheet.png"]
     self.img:setFilter('nearest', 'nearest')
     self.wallq = love.graphics.newQuad(12, 12, 12, 12, self.img:getDimensions())
+    self.spritebatch = love.graphics.newSpriteBatch(self.img, 1000)
     tilemp.bind('v', self.img, love.graphics.newQuad(0, 2 * 12, 12, 12, self.img:getDimensions()))
     tilemp.bind('b', self.img, love.graphics.newQuad(1 * 12, 2 * 12, 12, 12, self.img:getDimensions()))
     tilemp.bind('n', self.img, love.graphics.newQuad(2 * 12, 2 * 12, 12, 12, self.img:getDimensions()))
-    tilemp.bind('|', self.img, love.graphics.newQuad(0, 12, 12, 12, self.img:getDimensions()))
+    tilemp.bind('>', self.img, love.graphics.newQuad(0, 12, 12, 12, self.img:getDimensions()))
+    tilemp.bind('|', self.img, love.graphics.newQuad(2 * 12, 12, 12, 12, self.img:getDimensions()))
     tilemp.bind('_', self.img, love.graphics.newQuad(0, 0, 12, 12, self.img:getDimensions()))
+    tilemp.bind('o', self.img, love.graphics.newQuad(3 * 12, 0, 12, 12, self.img:getDimensions()))
+    tilemp.bind('p', self.img, love.graphics.newQuad(4 * 12, 0, 12, 12, self.img:getDimensions()))
+    tilemp.bind('k', self.img, love.graphics.newQuad(3 * 12, 12, 12, 12, self.img:getDimensions()))
+    tilemp.bind('l', self.img, love.graphics.newQuad(4 * 12, 12, 12, 12, self.img:getDimensions()))
+    tilemp.bind('q', self.img, love.graphics.newQuad(0 * 12, 3 * 12, 12, 12, self.img:getDimensions()))
+    tilemp.bind('w', self.img, love.graphics.newQuad(1 * 12, 3 * 12, 12, 12, self.img:getDimensions()))
+    tilemp.bind('e', self.img, love.graphics.newQuad(2 * 12, 3 * 12, 12, 12, self.img:getDimensions()))
     tilemp.parse('res/level.txt')
+    for x=0, 2000, 12 do
+        for y=0, 2000, 12 do
+            self.spritebatch:add(self.wallq, x, y)
+        end
+    end
 end
 
 function Level:draw()
-    for x=0, 2000, 12 do
-        for y=0, 2000, 12 do
-            --love.graphics.draw(self.img, self.wallq, x, y)
-        end
-    end
+    love.graphics.draw(self.spritebatch)
     tilemp.drawTiles()
     love.graphics.rectangle('line', self.pworld:getRect(self.rect))
 end
